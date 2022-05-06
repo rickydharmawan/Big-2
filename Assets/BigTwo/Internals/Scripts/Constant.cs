7a61f098c4e39e670afa87b5441633761e1524f4
Ricky <ricky@wonderednow.com>
2022-05-06 18:01:06 +0700

    Add Constant class to store all of the constant value

---
+++ Assets/BigTwo/Internals/Scripts/Constant.cs
@@ -0,0 +1,13 @@
+namespace BigTwo
+{
+    public class Constant
+    {
+        public const int CARD_MAXIMUM_COUNT = 52;
+
+        public const int CARD_RANK_MIN = 1;
+        public const int CARD_RANK_MAX = 13;
+
+        public const int CARD_SUITE_MIN = 1;
+        public const int CARD_SUITE_MAX = 4;
+    }
+}
