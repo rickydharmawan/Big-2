3930eeaf5782d82c6f1bc876a5dd53fe89c54fdb
Ricky <ricky@wonderednow.com>
2022-05-06 19:23:42 +0700

    Add Brain and States base class

---
+++ Assets/BigTwo/Internals/Scripts/Bot/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace BigTwo.Bot
+{
+    public interface IState<T>
+    {
+        void OnEnter(Brain<T> brain, T t);
+        void OnUpdate(Brain<T> brain, T t);
+        void OnExit(Brain<T> brain, T t);
+    }
+}
